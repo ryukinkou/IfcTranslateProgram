50c0c6f5e3c82f3f0861feac4b45d997686f6737
ryukinkou <ryukinkou@gmail.com>
2014-05-10 10:17:00 +0800

    nothing but reorganize sth.

--- src/cn/liujinhang/paper/ifc/Translation.java
+++ src/cn/liujinhang/paper/ifc/Translation.java
@@ -13,49 +13,51 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-public class IFC2OWLConverter {
+public class Translation {
 
 	public static void main(String[] args) throws Throwable {
 
-		System.out.println("read xsl start");
-//		InputStreamReader xslFileReader = new InputStreamReader(
-//				new URL(
-//						"http://rhizomik.net/redefer-services/xsl/xsd2owl.xsl")
-//						.openStream());
-		
-		Source xslSource = new StreamSource(new File("/Users/RYU/git/IfcTranslateProgram/xsl/xsd2owl.xsl"));
-		System.out.println("read xsl end");
-		
-		System.out.println("read xsd start");
+		System.out.println("xsl loading start");
+
+		Source xslSource = new StreamSource(new File(
+				System.getProperty("user.dir") + "\\ifc\\xsd2owl.xsl"));
+		System.out.println("xsl loading end");
+
+		System.out.println("xsd loading start");
+
+		// InputStreamReader xsdFileReader = new InputStreamReader(
+		// new FileInputStream(System.getProperty("user.dir")
+		// + "\\ifc\\ifcXML4.xsd"));
+
 		InputStreamReader xsdFileReader = new InputStreamReader(
 				new URL(
 						"http://www.buildingsmart-tech.org/ifcXML/IFC4/final/ifcXML4.xsd")
 						.openStream());
 
 		Source xsdSource = new StreamSource(xsdFileReader);
-		System.out.println("read xsd end");
-		
+		System.out.println("xsd loading end");
+
 		TransformerFactory factory = TransformerFactory.newInstance(
 				"net.sf.saxon.TransformerFactoryImpl", null);
 		Transformer transformer = factory.newTransformer(xslSource);
 		StringWriter writer = new StringWriter();
 
-		System.out.println("transform start");
+		System.out.println("translation start");
 		transformer.transform(xsdSource, new StreamResult(writer));
-		System.out.println("transform end");
+		System.out.println("translation end");
 
-		System.out.println("write file start");
+		System.out.println("file output start");
 		String result = new String(writer.getBuffer());
 		result = result.replaceAll("&amp;", "&");
 
-		File file = new File(
-				"/Users/RYU/git/IfcTranslateProgram/xsl/ifcXML4.xml");
+		File file = new File(System.getProperty("user.dir")
+				+ "\\ifc\\ifcOWL.owl");
 
 		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
 		bufferedWriter.write(result);
 		bufferedWriter.flush();
 		bufferedWriter.close();
-		System.out.println("write file end");
+		System.out.println("file output end");
 
 	}
 
