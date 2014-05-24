daf9f633e281818b25cc9596704ea3dc4321fb51
ryukinkou <ryukinkou@gmail.com>
2014-05-24 15:52:53 +0800

    let's do this.

--- src/cn/liujinhang/paper/ifc/LetsDoThis.java
+++ src/cn/liujinhang/paper/ifc/LetsDoThis.java
@@ -13,7 +13,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-public class Translation {
+public class LetsDoThis {
 
 	public static void main(String[] args) throws Throwable {
 
